7855a6a3e17b124de1935b6c37f4d48feefcb0ca
Thyago De Luca Silva Costa <deluca42@gmail.com>
2013-03-26 12:07:43 -0300

    first commit
    criação do projeto

---
+++ Vital.Exercicio.GerenciadorDeArquivo/GerenciadorDeArquivo.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vital.Exercicio.GerenciadorDeArquivo
+{
+    public class Class1
+    {
+    }
+}
