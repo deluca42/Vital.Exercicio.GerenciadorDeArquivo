d81037ec68fba0636369d868de3277ab0dd41a23
Thyago De Luca Silva Costa <deluca42@gmail.com>
2013-03-26 12:19:45 -0300

    primeiro teste

--- Vital.Exercicio.GerenciadorDeArquivoTest/GerenciadorDeArquivoTest.cs
+++ Vital.Exercicio.GerenciadorDeArquivoTest/GerenciadorDeArquivoTest.cs
@@ -3,10 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NUnit.Framework;
 
 namespace Vital.Exercicio.GerenciadorDeArquivoTest
 {
+    [TestFixture]
     public class GerenciadorDeArquivoTest
     {
+        [Test]
+        public void teste_de_insacia()
+        {
+            
+        }
     }
 }
